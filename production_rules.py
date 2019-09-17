178fe72ed8c31b5772c87e84aff6edbb2a17ff4b
Stephanie Warman <swarman2@gulls.salisbury.edu>
2019-09-17 08:49:39 -0400

    added production rules with hardcoded angle

--- production_rules.py
+++ production_rules.py
@@ -1,1 +1,12 @@
 # Placeholder for future production rule functions
+import math
+from pointer_class import *
+def Ff(obj):
+  x,y=obj.get_pos()
+  obj.set_pos((x+math.cos(obj.get_angle()),y+math.sin(obj.get_angle())))
+def rotation(obj,clockwise):
+  if clockwise==1:
+    obj.set_angle(obj.get_angle()-math.pi/4)
+  else:
+    obj.set_angle(obj.get_angle() + math.pi/4)
+  
