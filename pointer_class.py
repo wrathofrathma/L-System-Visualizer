28219b1e7ee9af9dd078c9ad5855601e0efb5f23
Stephanie Warman <swarman2@gulls.salisbury.edu>
2019-09-19 08:57:32 -0400

    added member for initial position

--- pointer_class.py
+++ pointer_class.py
@@ -4,6 +4,7 @@ class pointer_class:
   #this class holds position and angle
   def __init__(self, x, y):
     pos = np.array([x,y])
+    self.init_pos = pos
     self.pos= pos
     self.angle = 0
   def print_info(self):
