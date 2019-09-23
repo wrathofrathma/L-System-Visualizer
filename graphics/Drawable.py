51254feb5d4426164f9e6d4a7199943d0effe252
Rathma <wrathofrathma@gmail.com>
2019-09-22 23:58:49 -0400

    Shifting around virtual methods

--- graphics/Drawable.py
+++ graphics/Drawable.py
@@ -8,33 +8,22 @@ class Drawable():
         self.VBO = glGenBuffers(1)
          # Vertex buffer ID
         self.vPos=0  # Shader vertex data position
-        self.vNorm=1 # Shader vertex normal position
-        self.vColor=2 # Shader vertex color position
-        self.uModel = GLuint(0) # Shader position of the model matrix
+        self.vColor=1 # Shader vertex color position
+        self.vNorm=2 # Shader vertex normal position. For 3D Lighting later.
+
+        self.uModel = GLuint(0) # Shader position of the model matrix. For later when we do 3D meshes and rotations.
         self.vertices = np.array([]) # Placeholder for vertices of our mesh
         self.shader = None # Shader object
         self.update = True # Update flag for updating our GPU data. Called in our draw function.
 
     def draw(self):
-        pass
+        print("[ ERROR ] Define the draw function.")
 
+    # Uploads the vertices and color data to the GPU. Virtual so that it's easy to differentiate between 2D/3D later.
     def updateGPU(self):
-        print("Updating GPU")
-        self.update=False
-        glBindVertexArray(self.VAO)
-        # Allocate space for data
-        glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
-        # Upload data
-        glBufferData(GL_ARRAY_BUFFER, ArrayDatatype.arrayByteCount(self.vertices), self.vertices, GL_DYNAMIC_DRAW)
+        print("[ ERROR ] Define the update function.")
 
-        # Tell opengl how to work this data
-        glVertexAttribPoiner(self.vPos, 2, GL_FLOAT, GL_FALSE, 0, None)
-        glEnableVertexAttribArray(self.vPos)
-
-        # Unbind buffers
-        glBindBuffer(GL_ARRAY_BUFFER, 0)
-        glBindVertexArray(0)
-        print("Finished update")
+    # Deletes the data from the GPU. Should be the same for 2D & 3D
     def cleanup(self):
         GL.glDeleteVertexArrays(1, self.VAO)
         GL.glDeleteBuffers(1, self.VBO)
@@ -43,8 +32,9 @@ class Drawable():
         self.shader = shader
 
     def setVertices(self, vertices):
-        self.vertices = vertices
+        self.vertices = np.array(vertices, dtype=np.float32)
         self.update=True
+
 
     def getVertices(self):
         return self.vertices
--- graphics/Mesh.py
+++ graphics/Mesh.py
@@ -1,4 +1,5 @@
 from OpenGL import GL
+from OpenGL.GL import *
 import numpy as np
 from graphics.Drawable import *
 
@@ -12,6 +13,50 @@ class Mesh(Drawable):
 
     # TODO Fix the framerate on this. Seems to only update when PyQT wants us to.
     def draw(self):
+        if(len(self.vertices)==0):
+            print("[ ERROR ] Empty vertices")
+            return 0
         if(self.update):
             self.updateGPU()
+        if(self.shader!=None):
+            # Bind shader & VAO
+            self.shader.bind()
+            glBindVertexArray(self.VAO)
+            # Draw
+            print(len(self.vertices) / 2)
+            glDrawArrays(GL_TRIANGLES, 0, 3)
+            # Unbind shader & VAO
+            glUseProgram(0)
+            glBindVertexArray(0)
+
         print("Drawing Update")
+
+
+    def updateGPU(self):
+        print("[ INFO ] Updating GPU")
+        if(self.shader==None):
+            print("[ ERROR ] Shader not set, aborting update.")
+            return
+        if(len(self.vertices)==0):
+            print("[ ERROR ] Vertice count = 0, aborting update.")
+            return
+
+        # Get sizes of things
+        vertice_size = ArrayDatatype.arrayByteCount(self.vertices)
+
+        self.shader.bind()
+        self.update=False
+
+        glBindVertexArray(self.VAO)
+        # Allocate space for data
+        glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
+        # Upload data
+        glBufferData(GL_ARRAY_BUFFER, vertice_size, self.vertices, GL_DYNAMIC_DRAW)
+
+        # Tell opengl how to work this data
+        glVertexAttribPointer(self.vPos, 2, GL_FLOAT, GL_FALSE, 0, None)
+        glEnableVertexAttribArray(self.vPos)
+        # Unbind buffers
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        glBindVertexArray(0)
+        print("[ INFO ] Finished update")
