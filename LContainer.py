e6b62971fe4217b8518408a82db9bbc07606a2a5
Rathma <wrathofrathma@gmail.com>
2019-09-22 23:58:30 -0400

    Some shader loading changes

--- LContainer.py
+++ LContainer.py
@@ -6,20 +6,31 @@ class LContainer():
 
     def split(self):
         self.meshes.append(Mesh())
+        self.meshes[-1].setShader(self.shader)
 
     def add_vertex(self, vertex):
         print("Adding vertex: " + str(vertex))
         if(len(self.meshes)==0):
             self.meshes.append(Mesh())
+            self.meshes[-1].setShader(self.shader)
 
         vertices = self.meshes[-1].getVertices()
         vertices = np.append(vertices, vertex)
         print(vertices)
         self.meshes[-1].setVertices(vertices)
+        print("Post updated vertices: " + str(self.meshes[-1].getVertices()))
 
     def cleanup(self):
-        pass
+        for mesh in self.meshes:
+            mesh.cleanup()
 
     def draw(self):
+        count = 0
         for mesh in self.meshes:
             mesh.draw()
+            count+=1
+
+    def setShader(self, shader):
+        self.shader = shader
+        for mesh in self.meshes:
+            mesh.setShader(shader)
--- LSystemWidget.py
+++ LSystemWidget.py
@@ -7,7 +7,8 @@ from graphics.Shader import *
 import numpy as np
 from PyQt5.QtWidgets import *
 
-
+from graphics.Mesh import *
+from graphics.Quad import *
 ### TODO
 # Shader loading
 # Mesh drawing
@@ -24,17 +25,17 @@ class LSystemDisplayWidget(QOpenGLWidget):
         self.color = np.array([0.0, 0.3, 0.0, 1.0])
         self.wireframe=True
         self.meshes = LContainer()
-        self.meshes.add_vertex(np.array([0.0,0.0]))
-        self.meshes.add_vertex(np.array([0.0,1.0]))
-        self.meshes.add_vertex(np.array([1.0,1.0]))
-
+        self.quad = Quad()
     # Virtual functions inherited by QOpenGLWidget
 
     # Called whenever we want to update the widget
     def paintGL(self):
         GL.glClearColor(self.color[0], self.color[1], self.color[2], self.color[3])
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
-        self.meshes.draw()
+        print("Number of meshes: " + str(len(self.meshes.meshes)))
+        #self.meshes.draw()
+        #self.mesh.draw()
+        self.quad.draw()
         self.printOpenGLErrors()
 
     # Catch opengl errors between every draw.
@@ -43,16 +44,33 @@ class LSystemDisplayWidget(QOpenGLWidget):
 
     # Sets up viewport, projection, other resize shenanigans
     def resizeGL(self, w, h):
-        pass
+        print("[ INFO ] OpenGL Resized: " + str(w) + "," + str(h))
+        glViewport(0,0,w,h)
 
     # CAlled once before resizeGL and paintGL.
     def initializeGL(self):
+        print("[ INFO ] Initializing OpenGL...")
         self.loadShaders()
         GL.glClearColor(self.color[0], self.color[1], self.color[2], self.color[3])
         GL.glEnable(GL.GL_DEPTH_TEST)
         GL.glDepthFunc(GL.GL_LESS)
+        # Testing mesh stuff.
+        self.quad.setShader(self.shader)
+        self.mesh = Mesh()
+        self.mesh.setShader(self.shader)
+        vertices = np.array([0.0,0.0,0.001,0.0,0.0,0.001], dtype=np.float32)
+        self.mesh.setVertices(vertices)
+        #ogl.meshes.add_vertex(np.array([0.0,0.0], dtype=np.float32))
+        #ogl.meshes.add_vertex(np.array([0.0,0.2], dtype=np.float32))
+        #ogl.meshes.add_vertex(np.array([0.2,0.0], dtype=np.float32))
+
+        #ogl.meshes.add_vertex(np.array([0.0,-1.0]))
+        #ogl.meshes.add_vertex(np.array([0.0,0.0]))
+        #ogl.meshes.add_vertex(np.array([0.5,0.5]))
+        #ogl.meshes.add_vertex(np.array([0.5,0.0]))
 
     def toggleWireframe(self):
+        print("[ INFO ] Toggling wireframe.")
         if(self.wireframe):
             GL.glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
             self.wireframe = False
@@ -62,12 +80,14 @@ class LSystemDisplayWidget(QOpenGLWidget):
 
     # Cleanup code for OpenGL. We need to cleanup our mesh objects and shaders from GPU memory or it'll leak.
     def cleanup(self):
+        print("[ INFO ] Cleaning up display widget memory.")
         self.meshes.cleanup()
 
     def loadShaders(self):
+        print("[ INFO ] Loading shaders...")
         self.shader = Shader("assets/shaders/Default.vs", "assets/shaders/Default.fs")
-
-        print("Yeah bitch")
+        self.meshes.setShader(self.shader)
+        print("[ INFO ] Shaders loaded.")
 
 if __name__ == "__main__":
     app = QApplication([])
@@ -80,6 +100,7 @@ if __name__ == "__main__":
     #layout.addWidget(l1)
     layout.addWidget(ogl)
     #layout.addWidget(l2)
+
 
     window.setLayout(layout)
     window.show()
