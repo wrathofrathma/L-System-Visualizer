8f4c900e12cd852748a3f033cf76c1e67db16262
mattythats <54945408+mattythats@users.noreply.github.com>
2019-10-06 10:53:33 -0400

    Add files via upload
    added custom QMainWindow class to add menubar with screenshot function

--- UI.v0.02.py
+++ UI.v0.02.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QLineEdit, QTextEdit, QGridLayout, QApplication, QLabel
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QLineEdit, QTextEdit, QGridLayout, QApplication, QLabel, QAction
 import sys
 from PyQt5 import QtWidgets, QtCore
 import numpy as np
@@ -73,9 +73,6 @@ class UIWidget(QWidget):
     self.exitbutton = QPushButton("Exit", self)
     self.exitbutton.clicked.connect(self.cleanup)
 
-    self.screenshotbutton = QPushButton("Save a photo of your L System", self);
-    self.screenshotbutton.clicked.connect(lambda: self.takeScreenshot());
-
     #Adding widgets to window
     self.layout.addWidget(self.axiom, 1, 0)
     self.layout.addWidget(self.axiomEdit, 1, 1, 1, 3)
@@ -90,12 +87,11 @@ class UIWidget(QWidget):
     self.layout.addWidget(self.cantor, 9, 0, 1, 1)
     self.layout.addWidget(self.graphix, 8, 1, 5, -1)
     self.layout.addWidget(self.lsysbutton, 10, 0, 1, 1)
-    self.layout.addWidget(self.screenshotbutton, 11, 0, 1, 1)
     self.layout.addWidget(self.exitbutton, 12, 0, -1, 1)
 
     self.setLayout(self.layout)
     self.setGeometry(500, 500, 500, 500)
-    self.show()
+    #self.show()
 
   def cleanup(self):
       ''' This function cleans the graphics '''
@@ -277,12 +273,20 @@ class UIWidget(QWidget):
     self.genLSys()
     #print(example)
 
-  def takeScreenshot(self):
-    self.graphix.screenshot()
-
-
+class Window(QMainWindow):
+  def __init__(self, parent=None):
+    super(Window,self).__init__(parent=parent)
+    self.ui_widget = UIWidget()
+    self.setCentralWidget(self.ui_widget)
+    bar = self.menuBar()
+    ss_action = QAction('Take Screenshot', self)
+    bar.addAction(ss_action)
+    ss_action.triggered.connect(lambda: self.ui_widget.graphix.screenshot())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ui = UIWidget()
+    win = Window()
+    win.resize(500,500)
+    win.show()
+    #ui = UIWidget()
     sys.exit(app.exec_())
