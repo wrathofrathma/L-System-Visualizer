21a713a7685e0643d785518157127a8eda23cefd
Rathma <wrathofrathma@gmail.com>
2019-10-15 09:11:44 -0400

    Added colors, removed default gen, going to add a timer for the colors.

--- assets/shaders/2DShader.fs
+++ assets/shaders/2DShader.fs
@@ -3,6 +3,13 @@
 in vec4 frag_color;
 out vec4 FragColor;
 
+uniform float time;
+
 void main(){
+  vec4 c1 = vec4(1.0,0.0,1.0,0.0);
+  vec4 c2 = vec4(0.0,1.0,0.0,1.0);
+
+  float r = mod(time, 1);
+  //FragColor = mix(c1,c2,0.5);
   FragColor = frag_color;
 }
