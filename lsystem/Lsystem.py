58e7caaa70202dc6df1547f9300476263043e9d2
mattblairro <54944275+mattblairro@users.noreply.github.com>
2019-10-17 10:14:16 -0400

    Fixed nondetermnism

--- lsystem/Lsystem.py
+++ lsystem/Lsystem.py
@@ -1,37 +1,125 @@
 import threading
+import random
+
+random.seed()
+def weightedrand(weights):
+    sums = 0
+    
+    numlist = []
+    for weight in weights:
+        sums = sums + weight
+        
+    if sums != 1:
+        print("error")
+    
+    rand = random.randint(0,99)
+    print(rand)
+    it = 0
+    for weight in weights:
+        wei = weight * 100
+        num = int(wei)
+        for i in range(num):
+            numlist.append(it)
+        
+        it= it+1
+    #print(numlist)
+    it= numlist[rand]    
+    #print(it)
+    return it
+        
+
 
 def axigen(axioms, rules):
-    for axiom in axioms:
-        newaxi=''
-        if axiom in rules:
-            newaxi += rules[axiom]
+     newaxi=''
+     weights = []
+     nurules = {}
+     strn = ""
+     #print (rules)
+
+     for i in rules:
+         temp = rules[i]
+         #print(i)
+         #print(rules[i])
+         for it in temp:
+             ntemp = it
+             weights.append(ntemp[0])
+             strn += ntemp[1]
+             strn += "~"
+         #print(strn)    
+         strn = strn[:-1] 
+         #print(strn)
+         nurules[i] = strn
+         #print(nurules)
+             
+     for axiom in axioms:
+        #print(axiom)
+        #print(axioms)
+        
+        if axiom in nurules:
+            temp = nurules[axiom]
+            if '~' in temp:
+               ar = temp.split('~')
+               #print(ar)
+               rand=weightedrand(weights)
+               newaxi += ar[rand]
+            else:
+               newaxi += temp
         else:
             newaxi += axiom
-    axioms = newaxi
-    return axioms
+
+     axioms = newaxi   
+     return axioms
 
 def axigenq(axioms, rules):
-    newaxi=''
-    for axiom in axioms[0]:
-        if axiom in rules:
-            newaxi += rules[axiom]
+     newaxi=''
+     weights = []
+     nurules = {}
+     strn = ""
+     #print (rules)
+
+     for i in rules:
+         temp = rules[i]
+         #print(i)
+         #print(temp)
+         for it in temp:
+             ntemp = it
+             weights.append(ntemp[0])
+             strn += ntemp[1]
+             strn += "~"
+         #print(strn)    
+         strn = strn[:-1] 
+         #print(strn)
+         nurules[i] = strn
+         #print(nurules)
+    
+     for axiom in axioms[0]:
+        if axiom in nurules:
+            temp = nurules[axiom]
+            if '~' in temp:
+               ar = temp.split('~')
+               rand=weightedrand(weights)
+               #print(ar[rand])
+               newaxi += ar[rand]
+            else:
+               newaxi += temp
         else:
             newaxi += axiom
 
-    axioms[0] = newaxi
+     axioms[0] = newaxi
 
 def lgen(axioms, rules, it):
     '''
     Takes in an axiom set of rules and number of iterations and generates the new string
     '''
     for _ in range(it):
+   
         if (len(axioms)>1):
             axi1,axi2=[''],['']
             axi1[0],axi2[0] = axioms[:int(len(axioms)/2)], axioms[int(len(axioms)/2):]
             #if(len(axioms)%2==0):
-            #    axi1,axi2 = axioms[:len(axioms)//2], axioms[:len(axioms)//2]
+             #   axi1,axi2 = axioms[:len(axioms)//2], axioms[:len(axioms)//2]
             #else:
-            #    axi1,axi2 = axioms[:len(axioms)//2], axioms[:(len(axioms)//2)+1]
+             #   axi1,axi2 = axioms[:len(axioms)//2], axioms[:(len(axioms)//2)+1]
 
 
             thread1 = threading.Thread(target=axigenq, args=(axi1,rules))
@@ -49,11 +137,18 @@ def lgen(axioms, rules, it):
 
         else:
             axioms=axigen(axioms, rules)
-    #print(axioms)
+    
     return axioms
+
+#rules = {}
+#rules["F"]= [[1,"F-F+"]]
+#print(rules)
+#weights = [.4,.2,.4]
+#print(lgen("F", rules, 10))
+
 '''
 def stackgen(axi,rules, it):
-
+    
     Takes in the generated string and makes it into a stack
 
     axi= lgen(axi,rules,it)
