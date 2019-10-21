436b2f7443a63578dacff9af406c0e281081c839
Lela Bones <lbones1@gulls.salisbury.edu>
2019-10-20 21:38:04 -0400

    Starting to refactor Lsystem.py file first

--- lsystem/Lsystem.py
+++ lsystem/Lsystem.py
@@ -2,226 +2,142 @@ import threading
 import random
 import copy
 random.seed()
+
 def weightedrand(weights):
-    #print("weights = ",weights)
-    sums = 0
-    numlist = []
-    for weight in weights:
-        sums = sums + weight
+  numlist = []
+  if sum(weights) != 1:
+    print("Does not sum to 1")
+  rand = random.randint(0,99)
+  it = 0
+  for weight in weights:
+    wei = weight * 100
+    num = int(wei)
+    for i in range(num):
+      numlist.append(it)
 
-    if sums != 1:
-        print("error")
-
-    rand = random.randint(0,99)
-    #print(rand)
-    it = 0
-    for weight in weights:
-        wei = weight * 100
-        num = int(wei)
-        for i in range(num):
-            numlist.append(it)
-
-        it= it+1
-    #print("numlist = ",numlist)
-    #print(numlist)
-    it= numlist[rand]
-    #print(it)
-
-    return it
+    it= it+1
+  it= numlist[rand]
+  return it
 
 
-def axigen(axioms, rules):
-    newaxi=''
-    strn = ""
-    #print (rules)
-    newrules = {}
-    for i in rules.keys():
-        #print("i = ",i)
-        temp = rules[i]
-        #print("temp = ",temp)
-        #print(i)
-        #print(rules[i])
-        weights = []
-        strn =""
-        for it in temp:
-            #print("ntemp = ",it)
-            ntemp = it
-            weights.append(ntemp[0])
-            strn += ntemp[1]
-            strn += "~"
-        #print(strn)
-        #print("weights in axigen = ",weights)
-        strn = strn[:-1]
-        #print(strn)
-        newrules[i] = strn
-        #print(nurules)
-    #print("new rules = ",newrules)
-    """
-    str_temp is the current string
-    it will be used to keep track of what parts of the string still need proccessing
-    Context free implimentation idea:
-    1. sort the keys from longest to shortest
-    2. say the key is length n, if the first n letters of the string match the key, add rule[key] to the new string
-    3a.if the first n letters ever match a key remove the first n letters of the string
-    3b.default rule: a letter goes to itself
-    """
-    str_temp = copy.deepcopy(axioms)
-    maxKeyLength = max(rules.keys())
-    rules =copy.deepcopy(list(rules.keys()))
-    #sort the keys from longest to shortest
-    for j in range(len(rules)):
-      m = j
-      for i in range(j+1,len(rules)):
-          if len(rules[m])<len(rules[i]):
-              m = i
-      temp = rules[j]
-      rules[j]=rules[m]
-      rules[m] = temp
-    while len(str_temp)>0:
-      found = 0
-      for rule in rules:
-          #print("rule = ",rule)
-          if rule == str_temp[:len(rule)]:
-              temp = newrules[rule]
-              if '~' in temp:
-                 ar = temp.split('~')
-                 #print(ar)
-                 rand=weightedrand(weights)
-                 newaxi += ar[rand]
-              else:
-                newaxi+=temp
-              #print("og string = ",str_temp)
-              #print("temp = ",temp)
-              #print("new axi = ",newaxi)
-              str_temp =str_temp[len(rule):]
-              #print("new string = ",str_temp)
-              found = 1
-              break;
-      #default rule is a letter goes to itself
-      if found == 0:
-          newaxi+=str_temp[0]
-          str_temp=str_temp[1:]
-    """
-    for axiom in axioms:
-    #print(axiom)
-    #print(axioms)
-    #print(nurules)
-    if axiom in nurules:
-        temp = nurules[axiom]
+def stringParse(strings, prods):
+  newstr=''
+  strn = ""
+  newprods = {}
+  for i in prods.keys():
+    temp = prods[i]
+    weights = []
+    strn =""
+    for it in temp:
+      ntemp = it
+      weights.append(ntemp[0])
+      strn += ntemp[1]
+      strn += "~"
+    strn = strn[:-1]
+    newprods[i] = strn
+  """
+  str_temp is the current string
+  it will be used to keep track of what parts of the string still need proccessing
+  Context free implimentation idea:
+  1. sort the keys from longest to shortest
+  2. say the key is length n, if the first n letters of the string match the key, add rule[key] to the new string
+  3a.if the first n letters ever match a key remove the first n letters of the string
+  3b.default rule: a letter goes to itself
+ """
+  str_temp = copy.deepcopy(strings)
+  maxKeyLength = max(prods.keys())
+  prods =copy.deepcopy(list(prods.keys()))
+  #sort the keys from longest to shortest
+  for j in range(len(prods)):
+    m = j
+    for i in range(j+1,len(prods)):
+      if len(prods[m])<len(prods[i]):
+        m = i
+    temp = prods[j]
+    prods[j]=prods[m]
+    prods[m] = temp
+  while len(str_temp)>0:
+    found = 0
+    for rule in prods:
+      if rule == str_temp[:len(rule)]:
+        temp = newprods[rule]
         if '~' in temp:
-           ar = temp.split('~')
-           #print(ar)
-           rand=weightedrand(weights)
-           newaxi += ar[rand]
+          ar = temp.split('~')
+          rand=weightedrand(weights)
+          newstr += ar[rand]
         else:
-           newaxi += temp
+          newstr+=temp
+        str_temp = str_temp[len(rule):]
+        found = 1
+        break;
+      #default rule is a letter goes to itself
+    if found == 0:
+      newstr+=str_temp[0]
+      str_temp=str_temp[1:]
+  return newstr
+
+def multiStringParse(strings, prods):
+
+  newstr=''
+  weights = []
+  newprods = {}
+  strn = ""
+
+  for i in prods:
+    weights = []
+    strn= ""
+    temp = prods[i]
+    for it in temp:
+      ntemp = it
+      weights.append(ntemp[0])
+      strn += ntemp[1]
+      strn += "~"
+    strn = strn[:-1]
+    newprods[i] = strn
+
+  for char in strings[0]:
+    if char in newprods:
+      temp = newprods[char]
+      if '~' in temp:
+        ar = temp.split('~')
+        rand=weightedrand(weights)
+        newstr += ar[rand]
+      else:
+        newstr += temp
     else:
-        newaxi += axiom
-    """
-    axioms = newaxi
+      newstr += axiom
+  strings[0] = newstr
 
-    return axioms
+def lThread(strings, prods, it):
+  '''
+  Takes in an axiom set of prods and number of iterations and generates the new string
+  '''
+  if max(list(prods.keys()))==1:
+    context_free = 1
+  else:
+    context_free=0
+  for _ in range(it):
+    if (len(strings)>1) and context_free:
+      str1, str2 = [''], ['']
+      str1[0], str2[0] = strings[:int(len(strings)/2)], strings[int(len(strings)/2):]
 
-def axigenq(axioms, rules):
+      thread1 = threading.Thread(target=multiStringParse, args=(str1,prods))
+      thread2 = threading.Thread(target=multiStringParse, args=(str2,prods))
 
-     #print("in axigenq")
-     newaxi=''
-     weights = []
-     nurules = {}
-     strn = ""
-     #print (rules)
+      thread1.start()
+      thread2.start()
 
-     for i in rules:
-         weights = []
-         strn= ""
-         temp = rules[i]
-         #print(i)
-         #print(temp)
-         for it in temp:
-             ntemp = it
-             weights.append(ntemp[0])
-             strn += ntemp[1]
-             strn += "~"
-         #print(strn)
-         strn = strn[:-1]
-         #print(strn)
-         #print(nurules)
-         nurules[i] = strn
+      thread1.join()
+      thread2.join()
+      strings=str1[0]+str2[0]
 
-
-     for axiom in axioms[0]:
-        if axiom in nurules:
-            temp = nurules[axiom]
-            if '~' in temp:
-               ar = temp.split('~')
-               rand=weightedrand(weights)
-               #print(ar[rand])
-               newaxi += ar[rand]
-            else:
-               newaxi += temp
-        else:
-            newaxi += axiom
-
-     axioms[0] = newaxi
-
-def lgen(axioms, rules, it):
-    #print("rules = ",rules)
-    '''
-    Takes in an axiom set of rules and number of iterations and generates the new string
-    '''
-    if max(list(rules.keys()))==1:
-        context_free = 1
     else:
-        context_free=0
-    for _ in range(it):
-
-        if (len(axioms)>1) and context_free:
-            axi1,axi2=[''],['']
-            axi1[0],axi2[0] = axioms[:int(len(axioms)/2)], axioms[int(len(axioms)/2):]
-            #if(len(axioms)%2==0):
-             #   axi1,axi2 = axioms[:len(axioms)//2], axioms[:len(axioms)//2]
-            #else:
-             #   axi1,axi2 = axioms[:len(axioms)//2], axioms[:(len(axioms)//2)+1]
+      strings=stringParse(strings, prods)
+  return strings
 
 
-            thread1 = threading.Thread(target=axigenq, args=(axi1,rules))
-            thread2 = threading.Thread(target=axigenq, args=(axi2,rules))
-
-
-
-            thread1.start()
-            thread2.start()
-
-
-            thread1.join()
-            thread2.join()
-            axioms=axi1[0]+axi2[0]
-
-        else:
-            axioms=axigen(axioms, rules)
-    print("string = ",axioms)
-    return axioms
-
-#rules = {}
-#rules["F"]= [[.6,"F-f+"],[.4,"F-F"]]
-#rules["H"]= [[.5,"H+h"],[.5,"H-H"]]
-#print(rules)
-#weights = [.4,.2,.4]
-#print(lgen("FH", rules, 10))
-
-'''
-def stackgen(axi,rules, it):
-    Takes in the generated string and makes it into a stack
-    axi= lgen(axi,rules,it)
-    stack = []
-    it=0
-    for _ in range(len(axi)):
-        stack.append(axi[it])
-        it=it+1
-    return stack
 if __name__ == "__main__":
-    rules = {"F":"F+F--F+F"}
-    print(lgen('F',rules,5))
-    val = stackgen(rules)
-    print(val.pop())
-'''''
+  prods = {"F":"F+F--F+F"}
+  print(lgen('F',prods,5))
+  val = stackgen(prods)
+  print(val.pop())
--- lsystem/lsystem_utils.py
+++ lsystem/lsystem_utils.py
@@ -34,24 +34,9 @@ saved_file = "assets/lsystems/saved_lsystems.json"
 def generate_lsystem(grammar):
   grammar_copy= copy.deepcopy(grammar)
   print("[ INFO ] Generating L-System with the given grammar..." + str(grammar))
-  #grammar_copy['angle'] = grammar_copy['angle'] * math.pi/180.0 # Convert to radians.
   # Generate full production string.
-  s = lgen(grammar_copy['axiom'], grammar_copy['rules'], grammar_copy['iterations'])
+  s = lThread(grammar_copy['axiom'], grammar_copy['rules'], grammar_copy['iterations'])
   # Generate vertics
-  """
-  s = s.split('f')
-  vertsArray =[]
-  vertsArray.append(readStack(s[0], (0,0), grammar['angle']))
-  for i in range (1,len(s)):
-    vertsArray.append(readStack(s[i],verts[i-1][-1], grammar['angle']))
-  # Converts to usable normalized coordinates
-  for verts in vertsArray:
-    verts = np.array(verts, dtype=np.float32)
-    verts = verts.reshape(verts.shape[0],verts.shape[1])
-    verts = normalize_coordinates(verts)
-  return vertsArray #returns an array of arrays of vertices
-  """
-
   verts_arr_temp = readStack(s,(0,0),grammar_copy['angle'])
   verts_arr = []
   m = map(max,verts_arr_temp)
@@ -60,76 +45,62 @@ def generate_lsystem(grammar):
     verts = np.array(verts, dtype=np.float32)
     #print(verts)
     rows = len(verts)
-    #verts = verts.reshape(len(verts),2)
     verts = verts.reshape(verts.shape[0]*verts.shape[1])
     verts = normalize_coordinates(verts,m)
     verts_arr.append(verts)
-  """
-
-  verts = readStack(s,(0,0),grammar_copy['angle'])
-  print(verts)
-  verts = np.array(verts, dtype=np.float32)
-  verts = verts.reshape(verts.shape[0],verts.shape[1])
-  verts = normalize_coordinates(verts)
-  print(verts)
-  """
   return verts_arr
 
 # Saves a given lsystem to disk to "lsystem/saved_lsystems.json"
 # Overwrites any previous lsystem defined with the same key.
 def save_lsystem(key, grammar):
-    # Check if the file exists.
-    if(os.path.exists(saved_file)):
-        # If it does, then load all saved data and replace/insert the new data to the dict.
-        with open(saved_file, "r") as sfile:
-            saved = json.load(sfile)
-            saved[key] = grammar
-    else:
-        # if it doesn't exist, we just create a new dict
-        saved = {key : grammar}
-    # Then overwrite the file.
-    with open(saved_file,"w") as sfile:
-        json.dump(saved, sfile, indent=2)
+  # Check if the file exists.
+  if(os.path.exists(saved_file)):
+    # If it does, then load all saved data and replace/insert the new data to the dict.
+    with open(saved_file, "r") as sfile:
+      saved = json.load(sfile)
+      saved[key] = grammar
+  else:
+    # if it doesn't exist, we just create a new dict
+    saved = {key : grammar}
+  # Then overwrite the file.
+  with open(saved_file,"w") as sfile:
+    json.dump(saved, sfile, indent=2)
 
 # Returns a given lsystem's vertices & grammar from the dict. Or returns None.
 # The return type is a tuple, (verts, grammar)
 # Grammar is a dictionary the represents the axiom, production rules, angle, and iterations to be loaded into the UI.
 
 def get_saved_lsystem(key):
-    if key in saved_lsystems:
-        grammar = saved_lsystems[key]
-        #verts = generate_lsystem(grammar)
-        #return (verts, grammar)
-        return grammar
-    else:
-        print("[ ERROR ] No L-System loaded with key: " + str(key))
-        return None
+  if key in saved_lsystems:
+    grammar = saved_lsystems[key]
+    return grammar
+  else:
+    print("[ ERROR ] No L-System loaded with key: " + str(key))
+    return None
 
 # Loads predefined & saved lsystems from file.
 def load_saved_lsystems():
-    print("[ INFO ] Loading saved L-Systems from disk...")
+  print("[ INFO ] Loading saved L-Systems from disk...")
+  # Check if the file exists.
+  if(os.path.exists(predef_file)):
+    # If it does, then load it as a json object.
+    predef = json.load(open(predef_file, "r"))
+    # For every key(aka lsystem definition), add it to our saved lsystems.
+    for key in predef.keys():
+      saved_lsystems[key] = predef[key]
 
-
-    # Check if the file exists.
-    if(os.path.exists(predef_file)):
-        # If it does, then load it as a json object.
-        predef = json.load(open(predef_file, "r"))
-        # For every key(aka lsystem definition), add it to our saved lsystems.
-        for key in predef.keys():
-            saved_lsystems[key] = predef[key]
-
-    # Check if the file exists.
-    if(os.path.exists(saved_file)):
-        # If it does, then load it as a json object.
-        saved = json.load(open(saved_file, "r"))
-        # For every key(aka lsystem definition), add it to our saved lsystems.
-        for key in saved.keys():
-            saved_lsystems[key] = saved[key]
+  # Check if the file exists.
+  if(os.path.exists(saved_file)):
+    # If it does, then load it as a json object.
+    saved = json.load(open(saved_file, "r"))
+    # For every key(aka lsystem definition), add it to our saved lsystems.
+    for key in saved.keys():
+      saved_lsystems[key] = saved[key]
 
 # Normalizes the coordinates such that the largest vertice bound is 1 or -1.
 # We will remove this later when we have proper scaling/zooming.
 def normalize_coordinates(coords,m=0):
-    if m == 0:
-        m=coords.max()
-    coords = coords/m
-    return coords
+  if m == 0:
+    m=coords.max()
+  coords = coords/m
+  return coords
