6687cc5dd5713a279d120560b5b25c7f13e997a2
wrathofrathma <wrathofrathma@gmail.com>
2019-11-02 19:48:39 -0400

    Adding some predefined colors for ease of use

---
+++ lsystem/graphics/colors.py
@@ -0,0 +1,9 @@
+# Predefined colors. 
+from glm import vec4
+
+class Colors:
+    red = vec4(1,0,0,0)
+    blue = vec4(0,1,0,0)
+    green = vec4(0,0,1,0)
+    white = vec4(1,1,1,0)
+    black = vec4(0,0,0,0)
