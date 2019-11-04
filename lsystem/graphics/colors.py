62054162150f9e22247e267283664149a6f70ae2
wrathofrathma <wrathofrathma@gmail.com>
2019-11-04 04:01:22 -0500

    Added some more colors

--- lsystem/graphics/colors.py
+++ lsystem/graphics/colors.py
@@ -3,7 +3,11 @@ from glm import vec4
 
 class Colors:
     red = vec4(1,0,0,0)
-    blue = vec4(0,1,0,0)
-    green = vec4(0,0,1,0)
+    blue = vec4(0,0,1,0)
+    green = vec4(0,1,0,0)
     white = vec4(1,1,1,0)
     black = vec4(0,0,0,0)
+    yellow = vec4(1,1,0,0)
+    orange = vec4(1,0.5,0,0)
+    purple = vec4(0.8,0,1,0)
+    pink = vec4(1,0.8,0.945,0)
