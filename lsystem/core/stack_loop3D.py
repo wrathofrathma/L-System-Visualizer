46a90c0f5d70bbf1f26776907a078457211f170d
Stephanie Warman <swarman2@gulls.salisbury.edu>
2020-04-07 11:58:04 -0400

    Fixed cantor set 3D

--- lsystem/core/stack_loop3D.py
+++ lsystem/core/stack_loop3D.py
@@ -19,9 +19,10 @@ def read_substring(
     obj_container = []  # make sure it's empty
     current_point = curr_state['point']  # initalize starting point
     #Rotation matrix converts a vector in the abs frame to turtle frame
-
+    vertices = []
     orientation_mat= curr_state['orientation_mat']
     rotation=obj_ortientation = R.from_matrix(orientation_mat)
+    vertices.append(current_point)
     #new_obj = obj(pos=current_point,rotation=obj_ortientation.as_rotvec()) # append first object
     #obj_container.append(new_obj)
     '''First row of the rotation matirx is the turtles forward
@@ -36,13 +37,15 @@ def read_substring(
         if char == "F":
             old_point = current_point
             current_point = np.add(current_point, unit_step)
+            vertices.append(current_point)
             obj_ortientation = R.from_matrix(orientation_mat)
-            print("old_point = ",old_point,"  new_point = ",current_point)
-            print("euler angle = ",obj_ortientation.as_euler('xyz',degrees=True))
-            print("rotation vector = ",obj_ortientation.as_rotvec())
-            print()
+            #print("old_point = ",old_point,"  new_point = ",current_point)
+            #print("euler angle = ",obj_ortientation.as_euler('xyz',degrees=True))
+            #print("rotation vector = ",obj_ortientation.as_rotvec())
+            #print()
             new_obj = obj(start = old_point, end = current_point, rotation=obj_ortientation.as_rotvec())
             obj_container.append(new_obj)
+        #TODO: Fix H
         elif char == "H":
             new_point = np.add(new_point, np.multiply(unit_step,.5))
             rotation=obj_ortientation = R.from_matrix(orientation_mat)
@@ -84,9 +87,9 @@ def read_substring(
             rotation=R.from_rotvec(rotation_vector)
             orientation_mat = rotation.apply(orientation_mat)
             unit_step = orientation_mat[0]
-
+    curr_state['point'] = current_point
     # returns angle that string left off on, array of vertices, and the new angle
-    return orientation_mat, obj_container
+    return orientation_mat, obj_container, vertices
 
 
 def read_stack(stack, starting_pt, angle, obj):
@@ -143,15 +146,16 @@ def read_stack(stack, starting_pt, angle, obj):
             curr_state["orientation_mat"] = tmp_state[1]
             char.replace("]", "")
 
-        curr_state["orientation_mat"], array_of_objects = read_substring(
+        curr_state["orientation_mat"], array_of_objects, verts = read_substring(
             char,
             curr_state,
             angle,
             obj,
         )
-        #if len(array_of_objects) == 1:
+        #if not(len(array_of_objects) == 1):
         twoD_array_objects.append(array_of_objects)
-        curr_state["point"] = array_of_objects[-1].opts['position']
+        #print(curr_state['point'])
+        #curr_state["point"] = verts[-1]
     print("[ INFO ] Finshed finding vertices (", round(time() - t, 3), "s )")
     return twoD_array_objects
 
