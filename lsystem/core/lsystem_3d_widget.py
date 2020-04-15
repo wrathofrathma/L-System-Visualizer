b288dc76ed356ba32f66b982624a167426b1a0e5
rathma <wrathofrathma@gmail.com>
2020-04-15 14:57:41 -0400

    Switched to a singular mesh by dumping the list of meshes into a singular mesh

--- lsystem/core/lsystem_3d_widget.py
+++ lsystem/core/lsystem_3d_widget.py
@@ -1,6 +1,6 @@
 """This file handles the OpenGL Window actions"""
 from time import time
-
+import math
 # Other includes
 import numpy as np
 # Lsystem includes
@@ -10,7 +10,8 @@ from PIL import Image
 # PyQt5 includes
 from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
 
-from lsystem.graphics.square import Square
+from lsystem.graphics.mesh import MeshObject
+
 from pyqtgraph.opengl import GLLinePlotItem, GLMeshItem, GLViewWidget
 
 
@@ -20,7 +21,7 @@ class LSystem3DWidget(GLViewWidget):
         self.start_time = time()
 
         # Production scene objects.
-        self.mesh_list = []
+        self.mesh = None
         self.mesh_positions = []
         self.original_fov = self.opts['fov']
         self.original_distance = self.opts['distance']
@@ -42,11 +43,13 @@ class LSystem3DWidget(GLViewWidget):
     def screenshot(self, filename, pos):
         #print("[ INFO ] Intentionally broken at the moment. There is a bug in pyqtgraph's image exporter so we'll need to fork or do it ourselves")
         #rect is the rectange (x,y,width,height) of the widget
-        rect = self.frameGeometry()
-        #shift over to get absl. pos
-        rect.translate(pos)
-        im = ImageGrab.grab(bbox=(rect.x(), rect.y(), rect.x()+rect.width(), rect.y()+rect.height())) # X1,Y1,X2,Y2
-        im.save(filename)
+        # rect = self.frameGeometry()
+        # #shift over to get absl. pos
+        # rect.translate(pos)
+        # im = ImageGrab.grab(bbox=(rect.x(), rect.y(), rect.x()+rect.width(), rect.y()+rect.height())) # X1,Y1,X2,Y2
+        # im.save(filename)
+        self.grabFrameBuffer().save(filename)
+        # self.renderPixmap().save(filename)
 
     def cleanup(self):
         pass
@@ -55,19 +58,38 @@ class LSystem3DWidget(GLViewWidget):
         self.clear_meshes()
 
     def add_mesh(self, mesh_list):
+        """Sets the mesh of the scene to a combination of the list of meshes passed."""
+        meshes = []
         for m in mesh_list:
           for mesh in m:
             self.mesh_positions.append(mesh.opts["position"])
-            self.mesh_list.append(mesh)
-            self.addItem(mesh)
+            meshes.append(mesh)
+
+        self.mesh = self.combine_meshes(meshes)
+        self.addItem(self.mesh)
         self.center_meshes()
+
+
+    def combine_meshes(self, mesh_list: list):
+        """Combines mesh list into a singular mesh"""
+        # So can't we do this by tracking the offset of the vert list when we add the vertexes
+        # Then when we add the faces we just add the offset to each face?
+        verts = np.array([])
+        faces = np.array([])
+        for mesh in mesh_list:
+            offset = len(verts)
+            verts = np.append(verts, mesh.opts["vertexes"])
+            faces = np.append(faces, mesh.opts["indices"] + offset)
+            verts = verts.reshape(math.floor(verts.shape[0]/3),3)
+            faces = faces.reshape(math.floor(faces.shape[0]/3),3)
+        faces = faces.astype(int)
+        return MeshObject(vertexes=verts, indices=faces)
 
     def clear_meshes(self):
         """Removes all mesh objects from the scene"""
         self.mesh_positions.clear()
-        for mesh in self.mesh_list:
-            self.removeItem(mesh)
-        self.mesh_list.clear()
+        if(self.mesh is not None):
+            self.removeItem(self.mesh)
 
     def center_meshes(self):
       """Centers the entire mesh list on the origin"""
@@ -77,5 +99,4 @@ class LSystem3DWidget(GLViewWidget):
       avg_y = np.average(positions[:, 1])
       avg_z = np.average(positions[:, 2])
       center = (avg_x, avg_y, avg_z)
-      for mesh in self.mesh_list:
-        mesh.set_position(mesh.opts["position"] - center)
+      self.mesh.set_position(self.mesh.opts["position"] - center)
