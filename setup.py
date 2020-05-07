c408e737c320d7605dfd5921372623c9bb4c7307
rathma <wrathofrathma@gmail.com>
2020-05-06 22:14:49 -0400

    Cleaning some of the project packaging

--- setup.py
+++ setup.py
@@ -6,24 +6,22 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="L-System-Visualizer",
-    version="1.0.0",
+    version="1.0.6",
     author="Team BRAWT",
     author_email="lbones1@gulls.salisbury.edu",
     keywords="pyside2 qt5 lsystem",
     description="Generates visualizations of L Systems after a certain number of productions.",
     url="https://github.com/wrathofrathma/L-System-Visualizer.git",
     license="Unknown",
+    include_package_data=True,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Licence::TBD::TBD"
-        "Operating System::OS Independent"
     ],
-    install_requires=["pyside2", "matplotlib", "numpy", "Pillow", "PyGLM", "PyOpenGL", "pyparsing", "pyqtgraph", "scipy", "pyscreenshot"],
+    install_requires=["PyQt5", "matplotlib", "numpy", "Pillow", "PyGLM", "PyOpenGL", "pyparsing", "pyqtgraph", "scipy", "pyscreenshot", "pyrr", "pywavefront", ],
     py_modules=['lsystem'],
     python_requires='>=3.7',
     package_data={
       "": ["*.json"]
-      },
-    include_package_data=True
+      }
 )
